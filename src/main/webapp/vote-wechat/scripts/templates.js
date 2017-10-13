04c18f2795babc6036221fdc0c7a47cdda9c2e59
znb <zhunb319@gmail.com>
2017-10-14 00:21:23 +0800

    编译版本

--- src/main/webapp/vote-wechat/scripts/templates.js
+++ src/main/webapp/vote-wechat/scripts/templates.js
@@ -5,7 +5,9 @@
 
   return "<li>\r\n    <a href=\"\" class=\"zwfbox\" data-id=\""
     + container.escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"id","hash":{},"data":data}) : helper)))
-    + "\">\r\n      <img class=\"zwfimg\" src=\"/images/hcxz_zwf.png\"/>\r\n        <img class=\"pic\" src=\"/images/tip.jpg\"/>\r\n        <span class=\"number\">"
+    + "\">\r\n      <img class=\"zwfimg\" src=\"/images/hcxz_zwf.png\"/>\r\n        <img class=\"pic\" data-src=\""
+    + container.escapeExpression(((helper = (helper = helpers.img_src || (depth0 != null ? depth0.img_src : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"img_src","hash":{},"data":data}) : helper)))
+    + "\"/>\r\n        <span class=\"number\">"
     + container.escapeExpression(((helper = (helper = helpers.vote_code || (depth0 != null ? depth0.vote_code : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"vote_code","hash":{},"data":data}) : helper)))
     + "</span>\r\n        <div class=\"tt\">"
     + container.escapeExpression(((helper = (helper = helpers.username || (depth0 != null ? depth0.username : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"username","hash":{},"data":data}) : helper)))
@@ -25,21 +27,48 @@ templates["list/templates/order-li"] = template({"compiler":[7,">= 4.0.0"],"main
     return "";
 },"useData":true});
 templates["templates/mywork_list"] = template({"1":function(container,depth0,helpers,partials,data) {
-    var helper;
+    var stack1, helper;
 
   return "<li data-id=\""
     + container.escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"id","hash":{},"data":data}) : helper)))
-    + "\">\r\n    <a href=\"/detail.html\" class=\"zwfbox js_detail_a\">\r\n      <img class=\"zwfimg\" src=\"/images/hcxz_zwf.png\"/>\r\n        <img class=\"pic\" data-src=\""
+    + "\" data-status='"
+    + container.escapeExpression(((helper = (helper = helpers.proStatusName || (depth0 != null ? depth0.proStatusName : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"proStatusName","hash":{},"data":data}) : helper)))
+    + "'>\r\n    <a href=\"javascript:void(0);\" class=\"zwfbox js_detail_a\">\r\n      <img class=\"zwfimg\" src=\"/images/hcxz_zwf.png\"/>\r\n        <img class=\"pic\" data-src=\""
     + container.escapeExpression(((helper = (helper = helpers.img_src || (depth0 != null ? depth0.img_src : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"img_src","hash":{},"data":data}) : helper)))
     + "\"/>\r\n        <span class=\"number\">"
     + container.escapeExpression(((helper = (helper = helpers.vote_code || (depth0 != null ? depth0.vote_code : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"vote_code","hash":{},"data":data}) : helper)))
     + "</span>\r\n        <div class=\"tt\">"
     + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
-    + "</div>\r\n    </a>\r\n    <div class=\"vt_txt\"><span>票数："
+    + "</div>\r\n    </a>\r\n"
+    + ((stack1 = (helpers.equals || (depth0 && depth0.equals) || helpers.helperMissing).call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.proStatusName : depth0),"审核通过",{"name":"equals","hash":{},"fn":container.program(2, data, 0),"inverse":container.program(9, data, 0),"data":data})) != null ? stack1 : "")
+    + "</li>\r\n";
+},"2":function(container,depth0,helpers,partials,data) {
+    var stack1, helper;
+
+  return "    <div class=\"vt_txt\"><span>票数"
     + container.escapeExpression(((helper = (helper = helpers.num || (depth0 != null ? depth0.num : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"num","hash":{},"data":data}) : helper)))
-    + "票</span></div>\r\n    <div class=\"vt_txt\"><span>排名："
+    + "："
+    + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.num : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
+    + "票</span></div>\r\n"
+    + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.index : depth0),{"name":"if","hash":{},"fn":container.program(7, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"3":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return container.escapeExpression(((helper = (helper = helpers.num || (depth0 != null ? depth0.num : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"num","hash":{},"data":data}) : helper)));
+},"5":function(container,depth0,helpers,partials,data) {
+    return "0";
+},"7":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "    <div class=\"vt_txt\"><span>排名："
     + container.escapeExpression(((helper = (helper = helpers.index || (depth0 != null ? depth0.index : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"index","hash":{},"data":data}) : helper)))
-    + "</span></div>\r\n</li>\r\n";
+    + "</span></div>\r\n";
+},"9":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "     <div class=\"vt_txt\"><span>"
+    + container.escapeExpression(((helper = (helper = helpers.proStatusName || (depth0 != null ? depth0.proStatusName : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"proStatusName","hash":{},"data":data}) : helper)))
+    + "</span></div>\r\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
