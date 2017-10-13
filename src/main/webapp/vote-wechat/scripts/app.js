c68908d297b76b8abc0eac9d5705fee05148d154
znb <zhunb319@gmail.com>
2017-10-13 14:16:06 +0800

    微信

---
+++ src/main/webapp/vote-wechat/scripts/app.js
@@ -0,0 +1,393 @@
+(function() {
+  if (!this.require) {
+    var modules = {}, cache = {};
+
+    var require = function(name, root) {
+      var path = expand(root, name), indexPath = expand(path, './index'), module, fn;
+      module   = cache[path] || cache[indexPath];
+      if (module) {
+        return module;
+      } else if (fn = modules[path] || modules[path = indexPath]) {
+        module = {id: path, exports: {}};
+        cache[path] = module.exports;
+        fn(module.exports, function(name) {
+          return require(name, dirname(path));
+        }, module);
+        return cache[path] = module.exports;
+      } else {
+        throw 'module ' + name + ' not found';
+      }
+    };
+
+    var expand = function(root, name) {
+      var results = [], parts, part;
+      // If path is relative
+      if (/^\.\.?(\/|$)/.test(name)) {
+        parts = [root, name].join('/').split('/');
+      } else {
+        parts = name.split('/');
+      }
+      for (var i = 0, length = parts.length; i < length; i++) {
+        part = parts[i];
+        if (part == '..') {
+          results.pop();
+        } else if (part != '.' && part != '') {
+          results.push(part);
+        }
+      }
+      return results.join('/');
+    };
+
+    var dirname = function(path) {
+      return path.split('/').slice(0, -1).join('/');
+    };
+
+    this.require = function(name) {
+      return require(name, '');
+    };
+
+    this.require.define = function(bundle) {
+      for (var key in bundle) {
+        modules[key] = bundle[key];
+      }
+    };
+
+    this.require.modules = modules;
+    this.require.cache   = cache;
+  }
+
+  return this.require;
+}).call(this);this.require.define({"helper":function(exports, require, module){(function() {
+  var indexOf;
+
+  Handlebars.registerHelper("lt", function(a, b, options) {
+    if (a < b) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("eq", function(a, b, options) {
+    if (a === b) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("equals", function(a, b, options) {
+    if (a === b) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("json", function(jsonObj, options) {
+    return JSON.stringify(jsonObj);
+  });
+
+  Handlebars.registerHelper("is", function(a, test, options) {
+    if (a + "" === test + "") {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("isnt", function(a, test, options) {
+    if (a !== test) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("notin", function(a, arrayStr, options) {
+    var array, item, k, len;
+    array = arrayStr.split(",");
+    for (k = 0, len = array.length; k < len; k++) {
+      item = array[k];
+      if (a === item) {
+        return options.inverse(this);
+      }
+    }
+    return options.fn(this);
+  });
+
+  Handlebars.registerHelper("in", function(a, arrayStr, options) {
+    var array, item, k, len;
+    array = (arrayStr || "").split(",") || [];
+    for (k = 0, len = array.length; k < len; k++) {
+      item = array[k];
+      if (a === item) {
+        return options.fn(this);
+      }
+    }
+    return options.inverse(this);
+  });
+
+  Handlebars.registerHelper("nullDefault", function(a, b, options) {
+    if (a) {
+      return a;
+    } else {
+      return b;
+    }
+  });
+
+  Handlebars.registerHelper("has", function(a, b, options) {
+    if (a.indexOf(b) > -1) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("haslowwer", function(a, b, options) {
+    a = a || "";
+    a = a.toLowerCase();
+    if (a.indexOf(b) > -1) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("even", function(a, options) {
+    if (a % 2 === 0) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("odd", function(a, options) {
+    if (a % 2 === 1) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("add", function(a, b, options) {
+    return a + b;
+  });
+
+  Handlebars.registerHelper("multiply", function(a, b, options) {
+    return a * b;
+  });
+
+  Handlebars.registerHelper("before", function(a, b, arr, options) {
+    var i, j;
+    i = indexOf(arr, a);
+    j = indexOf(arr, b);
+    if (j > i) {
+      return options.inverse(this);
+    } else {
+      return options.fn(this);
+    }
+  });
+
+  indexOf = function(array, value) {
+    var a, i, k, len;
+    for (i = k = 0, len = array.length; k < len; i = ++k) {
+      a = array[i];
+      if (a === value) {
+        return i;
+      }
+    }
+  };
+
+  Handlebars.registerHelper("eachCount", function(a, options) {
+    var i, k, ref, result;
+    result = new Array();
+    for (i = k = 1, ref = a; 1 <= ref ? k <= ref : k >= ref; i = 1 <= ref ? ++k : --k) {
+      result.push(i);
+    }
+    return Handlebars.helpers["each"](result, options);
+  });
+
+  Handlebars.registerHelper("splitLast", function(a, options) {
+    var arr;
+    if (a === null) {
+      return "";
+    }
+    arr = a.split(",");
+    return arr[arr.length - 1];
+  });
+
+  Handlebars.registerHelper("gt", function(a, b, options) {
+    if (parseFloat(a) > parseFloat(b)) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
+
+  Handlebars.registerHelper("getValueInMap", function(map, key, options) {
+    var obj;
+    map = map || {};
+    for (obj in map) {
+      if (obj === key) {
+        return map[key];
+      }
+    }
+    return "";
+  });
+
+  Handlebars.registerHelper("getValueInList", function(objArr, value, options) {
+    var k, len, obj;
+    objArr = objArr || [];
+    for (k = 0, len = objArr.length; k < len; k++) {
+      obj = objArr[k];
+      if ((obj.name + "") === (value + "")) {
+        return obj.value;
+      }
+    }
+    return "";
+  });
+
+  Handlebars.registerHelper("getValueInListByKey", function(objArr, name, value, key, options) {
+    var k, len, obj;
+    objArr = objArr || [];
+    for (k = 0, len = objArr.length; k < len; k++) {
+      obj = objArr[k];
+      if ((obj[name] + "") === (value + "")) {
+        return obj[key];
+      }
+    }
+    return "";
+  });
+
+  Handlebars.registerHelper("size", function(arr, options) {
+    arr = arr || [];
+    return arr.length;
+  });
+
+  Handlebars.registerHelper("order", function(index, start, options) {
+    var num;
+    num = parseInt(index + 1) + start;
+    return num;
+  });
+
+  Handlebars.registerHelper("between", function(a, b, options) {
+    var k, numbers, results;
+    numbers = (function() {
+      results = [];
+      for (var k = a; a <= b ? k <= b : k >= b; a <= b ? k++ : k--){ results.push(k); }
+      return results;
+    }).apply(this);
+    return Handlebars.helpers["each"](numbers, options);
+  });
+
+  Handlebars.registerHelper("equalsAddOne", function(a, b, options) {
+    if (parseInt(a + 1) === parseInt(b)) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("equalsAdd", function(a, b, c, options) {
+    if (a === b + c) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  Handlebars.registerHelper("toLowwer", function(a, options) {
+    a = a || "";
+    return a.toLowerCase();
+  });
+
+}).call(this);
+;}});
+this.require.define({"openid":function(exports, require, module){var wxopenid=getcookie('wxopenid');  
+var key=getcookie('key');  
+if (key==''){  
+    var access_code=GetQueryString('code');  
+    if (wxopenid==""){  
+        if (access_code==null)  
+        {     
+            var fromurl=location.href;  
+            var url='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4871de30bcfb36bc&redirect_uri='+encodeURIComponent(fromurl)+'&response_type=code&scope=snsapi_base&state=STATE%23wechat_redirect&connect_redirect=1#wechat_redirect';  
+            location.href=url;  
+        }  
+        else  
+        {     
+            $.ajax({  
+                type:'get',  
+                url:ApiUrl+'/index.php?act=payment&op=getopenid',   
+                async:false,  
+                cache:false,  
+                data:{code:access_code},  
+                dataType:'json',  
+                success:function(result){                   
+                        if (result!=null && result.hasOwnProperty('openid') && result.openid!=""){  
+                           addcookie('wxopenid',result.openid,360000);                             
+                           getlogininfo(result.openid);  
+                        }   
+                        else  
+                        {  
+                          alert('微信身份识别失败 \n '+result);  
+                          location.href=fromurl;  
+                        }  
+                    }  
+                });      
+        }  
+    }else{  
+       if (key=='' && wxopenid!='')  
+           getlogininfo(wxopenid);    
+    }  
+
+    function getlogininfo(wxopenid){         
+        $.ajax({  
+           type:'get',  
+           url: ApiUrl + '/index.php?act=login&op=autologininfo',  
+           data: { wxopenid:wxopenid},  
+           dataType:'json',  
+           async:false,  
+           cache:false,                 
+           success: function (result) {                     
+                   if (result.return_code=='OK'){  
+                       addcookie('key',result.memberinfo.key);  
+                       addcookie('username',result.memberinfo.username);  
+                   }else{  
+                       alert(result.return_msg);  
+                       location.href=WapSiteUrl+'/tmpl/member/login.html';  
+                   }  
+           }  
+        });  
+    }  
+}  
+
+
+
+function addcookie(name,value,expireHours){  
+    var cookieString=name+"="+escape(value)+"; path=/";  
+    //判断是否设置过期时间  
+    if(expireHours>0){  
+        var date=new Date();  
+        date.setTime(date.getTime+expireHours*3600*1000);  
+        cookieString=cookieString+"; expire="+date.toGMTString();  
+    }  
+    document.cookie=cookieString;  
+}  
+  
+function getcookie(name){  
+    var strcookie=document.cookie;  
+    var arrcookie=strcookie.split("; ");  
+    for(var i=0;i<arrcookie.length;i++){  
+    var arr=arrcookie[i].split("=");  
+    if(arr[0]==name)return decodeURIComponent(arr[1]); //增加对特殊字符的解析  
+    }  
+    return "";  
+}  
+  
+function delCookie(name){//删除cookie  
+    var exp = new Date();  
+    exp.setTime(exp.getTime() - 1);  
+    var cval=getcookie(name);  
+    if(cval!=null) document.cookie= name + "="+cval+"; path=/;expires="+exp.toGMTString();  
+}  ;}});
---
+++ src/main/webapp/vote-wechat/scripts/templates.js
@@ -0,0 +1,83 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+  templates["list/templates/list"] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "<li>\r\n    <a href=\"/detail\" class=\"zwfbox\">\r\n      <img class=\"zwfimg\" src=\"/images/hcxz_zwf.png\"/>\r\n        <img class=\"pic\" src=\"/images/tip.jpg\"/>\r\n        <span class=\"number\">"
+    + container.escapeExpression(((helper = (helper = helpers.vote_code || (depth0 != null ? depth0.vote_code : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"vote_code","hash":{},"data":data}) : helper)))
+    + "</span>\r\n        <div class=\"tt\">"
+    + container.escapeExpression(((helper = (helper = helpers.username || (depth0 != null ? depth0.username : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"username","hash":{},"data":data}) : helper)))
+    + "</div>\r\n    </a>\r\n    <div class=\"name\"><span><i>"
+    + container.escapeExpression(((helper = (helper = helpers.num || (depth0 != null ? depth0.num : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"num","hash":{},"data":data}) : helper)))
+    + "</i>票</span><div class=\"pic_user\">\r\n        <img src=\"/images/tip.jpg\"/>"
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
+    + "</div></div>\r\n        <div class=\"btn\">\r\n        <input type=\"button\" datacode=\""
+    + container.escapeExpression(((helper = (helper = helpers.vote_code || (depth0 != null ? depth0.vote_code : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"vote_code","hash":{},"data":data}) : helper)))
+    + "\" class=\"btntp js_vote\" value=\"投票\"/>\r\n        <div class=\"numCon js_numsbox\"><button class=\"js_minus minus\">-</button>\r\n            <input type=\"text\" class=\"js_cart_addnum txt\" value=\"1\"  pattern=\"[0-9]*\"/><button class=\"js_plus plus\">+</button>\r\n        </div>\r\n    </div>\r\n</li>\r\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
+templates["list/templates/order-li"] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "";
+},"useData":true});
+templates["templates/mywork_list"] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "<li data-id=\""
+    + container.escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"id","hash":{},"data":data}) : helper)))
+    + "\">\r\n    <a href=\"/detail.html\" class=\"zwfbox js_detail_a\">\r\n      <img class=\"zwfimg\" src=\"/images/hcxz_zwf.png\"/>\r\n        <img class=\"pic\" data-src=\""
+    + container.escapeExpression(((helper = (helper = helpers.img_src || (depth0 != null ? depth0.img_src : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"img_src","hash":{},"data":data}) : helper)))
+    + "\"/>\r\n        <span class=\"number\">"
+    + container.escapeExpression(((helper = (helper = helpers.vote_code || (depth0 != null ? depth0.vote_code : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"vote_code","hash":{},"data":data}) : helper)))
+    + "</span>\r\n        <div class=\"tt\">"
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
+    + "</div>\r\n    </a>\r\n    <div class=\"vt_txt\"><span>票数："
+    + container.escapeExpression(((helper = (helper = helpers.num || (depth0 != null ? depth0.num : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"num","hash":{},"data":data}) : helper)))
+    + "票</span></div>\r\n    <div class=\"vt_txt\"><span>排名："
+    + container.escapeExpression(((helper = (helper = helpers.index || (depth0 != null ? depth0.index : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"index","hash":{},"data":data}) : helper)))
+    + "</span></div>\r\n</li>\r\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
+templates["templates/rank"] = template({"1":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = (helpers.lt || (depth0 && depth0.lt) || helpers.helperMissing).call(depth0 != null ? depth0 : {},(data && data.index),3,{"name":"lt","hash":{},"fn":container.program(2, data, 0),"inverse":container.program(4, data, 0),"data":data})) != null ? stack1 : "");
+},"2":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "  <li class=\"rank"
+    + container.escapeExpression((helpers.add || (depth0 && depth0.add) || helpers.helperMissing).call(depth0 != null ? depth0 : {},(data && data.index),1,{"name":"add","hash":{},"data":data}))
+    + "  js_list\">\r\n    <div class=\"mc\"><img src=\"/images/rank"
+    + container.escapeExpression((helpers.add || (depth0 && depth0.add) || helpers.helperMissing).call(depth0 != null ? depth0 : {},(data && data.index),1,{"name":"add","hash":{},"data":data}))
+    + ".png\"/></div>\r\n    <div class=\"name\"><img data-src=\""
+    + container.escapeExpression(((helper = (helper = helpers.img_src || (depth0 != null ? depth0.img_src : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"img_src","hash":{},"data":data}) : helper)))
+    + "\" alt=\"\" /><span>"
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
+    + "</span></div>\r\n    <div class=\"num\">"
+    + container.escapeExpression(((helper = (helper = helpers.num || (depth0 != null ? depth0.num : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"num","hash":{},"data":data}) : helper)))
+    + "票</div>\r\n  </li>\r\n";
+},"4":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "  <li>\r\n    <div class=\"mc\">"
+    + container.escapeExpression((helpers.add || (depth0 && depth0.add) || helpers.helperMissing).call(depth0 != null ? depth0 : {},(data && data.index),1,{"name":"add","hash":{},"data":data}))
+    + "</div>\r\n    <div class=\"name\"><img data-src=\""
+    + container.escapeExpression(((helper = (helper = helpers.img_src || (depth0 != null ? depth0.img_src : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"img_src","hash":{},"data":data}) : helper)))
+    + "\" alt=\"\" /><span>"
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)))
+    + "</span></div>\r\n    <div class=\"num\">"
+    + container.escapeExpression(((helper = (helper = helpers.num || (depth0 != null ? depth0.num : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"num","hash":{},"data":data}) : helper)))
+    + "票</div>\r\n  </li>\r\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<li class=\"rank_tit\">\r\n<div class=\"col1\">排名</div>\r\n<div class=\"col2\">作品名</div>\r\n<div class=\"col3\">当前票数</div>\r\n</li>\r\n"
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
+
+})();
